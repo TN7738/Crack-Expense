98845638565aef51463b5788df792ea507733515
TejasN <tj.tejas77@gmail.com>
2022-03-10 13:48:52 -0500

    added components for login

---
+++ app_public/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+    return (
+        <div className='header-wrap'>
+            
+        </div>
+    );
+};
+    
+    export default Header;
