3da64114e14027a77de427e2a02e2ccd5a8f15c3
TejasN <tj.tejas77@gmail.com>
2022-03-13 14:08:57 -0400

    login functionality

--- app_public/src/components/Loginbox/Loginbox.js
+++ app_public/src/components/Loginbox/Loginbox.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import './loginbox.scss';
+import axios from 'axios';
 
-const Loginbox = () => {
-    return (
-        <div className='loginbox-wrap'>
-            <div className='container'>
-                <div className='logo-wrap'>
-                    <img src="images/logo.png" alt="Logo" />
+class Loginbox extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    onChangeEmail(e){
+        this.setState({
+            email: e.target.value
+        });
+    }
+    onChangePassword(e){
+        this.setState({
+            password: e.target.value
+        });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        axios.get('http://localhost:3000/api/user')
+            .then(res => {
+                const foundEmail = res.data.some(el => el.email === userData.email);
+                const foundPassword = res.data.some(el => el.password === userData.password);
+                if(foundEmail && foundPassword){
+                    console.log("Logged in");
+                }
+            });
+    }
+    render(){
+        return (
+            <div className='loginbox-wrap'>
+                <div className='container'>
+                    <div className='logo-wrap'>
+                        <img src="images/logo.png" alt="Logo" />
+                    </div>
+                    <form onSubmit={this.onSubmit}>
+                        <input type="email" className="emailid" name="email" placeholder='Email' value={this.state.email} onChange={this.onChangeEmail} required />
+                        <input type="password" className="password" name="password" placeholder='Password' value={this.state.password} onChange={this.onChangePassword} required />
+                        <input type="submit" className="login-btn" value="Login"></input>
+                    </form>
                 </div>
-                <input type="text" className="emailid" name="emailid" placeholder='Email' />
-                <input type="password" className="password" name="password" placeholder='Password' />
-                <a className="login-btn">Log in</a>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
     
 export default Loginbox;
