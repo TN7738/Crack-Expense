17c422ae2cadaeceee2a83246a9d68c1b262bfea
TejasN <tj.tejas77@gmail.com>
2022-03-14 16:32:55 -0400

    changed class component to function for loginbox

--- app_public/src/components/Loginbox/Loginbox.js
+++ app_public/src/components/Loginbox/Loginbox.js
@@ -1,41 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './loginbox.scss';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 
-class Loginbox extends React.Component {
-    constructor(props) {
-        super(props);
+const Loginbox = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            email: '',
-            password: ''
-        }
-        
-    }
-    
-
-    onChangeEmail(e) {
-        this.setState({
-            email: e.target.value
-        });
-    }
-    onChangePassword(e) {
-        this.setState({
-            password: e.target.value
-        });
-    }
-
-    onSubmit(e) {
+    const onSubmit = (e) => {
         e.preventDefault();
 
         const userData = {
-            email: this.state.email,
-            password: this.state.password
+            email: email,
+            password: password
         };
 
         axios.get('http://localhost:3000/api/user')
@@ -53,24 +30,22 @@ class Loginbox extends React.Component {
                     console.log(cookies.get('user'));
                 }
             });
-    }
-    render() {
-        return (
-            <div className='loginbox-wrap'>
-                <div className='container'>
-                    <div className='logo-wrap'>
-                        <img src="images/logo.png" alt="Logo" />
-                    </div>
-                    <form onSubmit={this.onSubmit}>
-                        <input type="email" className="emailid" name="email" placeholder='Email' value={this.state.email} onChange={this.onChangeEmail} required />
-                        <input type="password" className="password" name="password" placeholder='Password' value={this.state.password} onChange={this.onChangePassword} required />
-                        <a className="signup-link" href="/signup">Not a member?</a>
-                        <input type="submit" className="login-btn" value="Login"></input>
-                    </form>
+    };
+    return (
+        <div className='loginbox-wrap'>
+            <div className='container'>
+                <div className='logo-wrap'>
+                    <img src="images/logo.png" alt="Logo" />
                 </div>
+                <form onSubmit={e => {onSubmit(e)}}>
+                    <input type="email" className="emailid" name="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} required />
+                    <input type="password" className="password" name="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} required />
+                    <a className="signup-link" href="/signup">Not a member?</a>
+                    <input type="submit" className="login-btn" value="Login"></input>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Loginbox;
