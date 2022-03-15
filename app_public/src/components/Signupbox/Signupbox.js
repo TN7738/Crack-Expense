f092c136229c03aed17e1ea2a14fd89f44c1c5ef
ParthDonga13 <80049125+ParthDonga13@users.noreply.github.com>
2022-03-15 17:57:05 -0400

    SignUPAPI

--- app_public/src/components/Signupbox/Signupbox.js
+++ app_public/src/components/Signupbox/Signupbox.js
@@ -1,18 +1,78 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './signupbox.scss';
+import axios from 'axios';
+import  { useHistory } from 'react-router-dom';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Signupbox = () => {
+const Signupbox = (props) => {
+
+    let history = useHistory();
+    toast.configure();
+    const [newuser, setnewuser] = useState({
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:''
+    });
+    const notify = (msg) =>{
+        toast(msg);
+    };
+
+    const handleChange = (e) =>{
+        const {name,value} = e.target;
+        setnewuser(previousState =>{
+            return {...previousState,[name]: value};
+        });
+    }
+
+    // const clearstate = () => {
+    //     setnewuser({...initalState});
+    // };
+
+    const createuser = (e) =>{
+        e.preventDefault();
+        const userData = {
+            firstName :newuser.firstName,
+            lastName : newuser.lastName,
+            email : newuser.email,
+            password : newuser.password
+        };
+
+        axios.get('http://localhost:3000/api/user')
+            .then(res => {
+                const foundEmail = res.data.some(el => el.email === userData.email);
+                if (foundEmail) { 
+                    notify('Email Already exist');
+                    newuser.email='';
+                }
+                else{
+                    axios.post('http://localhost:3000/api/user',userData)
+                        .then(
+                            res => {
+                                history.push("/");
+                                notify('Register Successfully');
+                            }
+                        )
+                }
+            });
+    };
+
     return (
         <div className='signupbox-wrap'>
             <div className='container'>
                 <div className='logo-wrap'>
                     <img src="images/logo.png" alt="Logo" />
                 </div>
-                <div className="title">Create an Account</div>
-                <input type="text" className="name" name="name" placeholder='Name' />
-                <input type="text" className="emailid" name="emailid" placeholder='Email' />
-                <input type="password" className="password" name="password" placeholder='Password' />
-                <a className="signup-btn">Sign Up</a>
+                <form onSubmit={createuser}>
+                    <div className="title">Create an Account</div>
+                    <input type="text" value={newuser.firstName} className="name" name="firstName" placeholder='First Name' onChange={handleChange} required/>
+                    <input type="text" value={newuser.lastName} className="name" name="lastName" placeholder='Last Name' onChange={handleChange} required/>
+                    <input type="text" value={newuser.email} className="emailid" name="email" id = "email" placeholder='Email' onChange={handleChange} required/>
+                    <input type="password" value={newuser.password} className="password" name="password" placeholder='Password' onChange={handleChange} required/>
+                    <a className="Login-link" href="/">Already a member?</a>
+                    <input type="submit" className="signup-btn" value="Signup"></input>
+                </form>
             </div>
         </div>
     );
