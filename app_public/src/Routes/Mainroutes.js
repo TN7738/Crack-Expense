fc46bc09dda110e909f7852c8b5a708cdaa3e2ad
Sandhya Paghdar <sandhyapaghdar@Sandhyas-Air.ht.home>
2022-03-16 12:39:29 -0400

    Homepage updated

--- app_public/src/Routes/Mainroutes.js
+++ app_public/src/Routes/Mainroutes.js
@@ -3,33 +3,24 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "../components/Login";
 import Signup from "../components/Signup";
-import Homepage from "../components/Homepage/Homepage";
+import Homepage from "../components/Homepage";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function MainRoutes() {
     return (
         <Router>
-            <Route exact path="/">
+            <Route exact path="/login">
                 <Login />
             </Route>
-            <Route path="/header">
-                <Header />
-            </Route>
-            <Route path="/footer">
-                <Footer />
-            </Route>
-            <Route path="/homepage">
+            <Route path="/">
                 <Homepage />
             </Route>
             <Route path="/signup">
                 <Signup />
-            </Route>
-            <Route path="/footer">
-                <Footer />
-            </Route>
+            </Route>       
         </Router>
     );
-}
+};
 
 export default MainRoutes;
