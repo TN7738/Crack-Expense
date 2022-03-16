1a7d51242bc2f251a7c4798300b6dd7e4a2737db
DhruviPatel2601 <dhruvipatel1667@gmail.com>
2022-03-16 15:53:26 -0400

    Homepage-touchup

--- app_public/src/components/Loginbox/Loginbox.js
+++ app_public/src/components/Loginbox/Loginbox.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './loginbox.scss';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
@@ -15,21 +15,21 @@ const Loginbox = () => {
             email: email,
             password: password
         };
-        
+
         axios.get('http://localhost:3000/api/user')
             .then(res => {
                 let foundFlag = false;
                 res.data.forEach(elem => {
-                    if(elem.email == userData.email && elem.password == userData.password){
+                    if (elem.email == userData.email && elem.password == userData.password) {
                         foundFlag = true;
-                        userData = {...userData, firstName: elem.firstName, lastName: elem.lastName};
+                        userData = { ...userData, firstName: elem.firstName, lastName: elem.lastName };
                         return;
                     }
                 });
-                if(foundFlag){
+                if (foundFlag) {
                     const cookies = new Cookies();
                     cookies.set('user', userData);
-                    history.push("/homepage");
+                    history.push("/");
                 }
             });
     };
@@ -39,7 +39,7 @@ const Loginbox = () => {
                 <div className='logo-wrap'>
                     <img src="images/logo.png" alt="Logo" />
                 </div>
-                <form onSubmit={e => {onSubmit(e)}}>
+                <form onSubmit={e => { onSubmit(e) }}>
                     <input type="email" className="emailid" name="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} required />
                     <input type="password" className="password" name="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} required />
                     <a className="signup-link" href="/signup">Not a member?</a>
