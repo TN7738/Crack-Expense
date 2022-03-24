cc02f03c7cc166e7ff4513c738488dd76324f2e8
ParthDonga13 <80049125+ParthDonga13@users.noreply.github.com>
2022-03-24 19:53:59 -0400

    logout functionality

--- app_public/src/components/Header/Header.js
+++ app_public/src/components/Header/Header.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import './header.scss';
 import Cookies from 'universal-cookie';
+import { toast } from 'react-toastify';
 
 const header = () => {
     const cookies = new Cookies();
+    toast.configure();
+    const notify = (msg) => {
+        toast(msg);
+    };
+
     let firstName;
     if(document.cookie.indexOf('user') != -1){
         firstName = cookies.get('user')['firstName'];
     }
+
+    const logout = () => {
+        cookies.remove('user');
+        notify("Logged out successfully");        
+    }
+
     const UlContent = props => {
         if(firstName == undefined){
             return <ul><li><a href="Login">Login</a></li><li><a href="Signup">Signup</a></li></ul>
         }
         else{
-            return <ul><li>Hi, {firstName}</li></ul>
+            return <ul><li>Hi, {firstName}</li><li><a href="/" onClick={logout}>Logout</a></li></ul>
         }
     }
     return (
